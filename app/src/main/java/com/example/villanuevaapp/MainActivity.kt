415a6def6d84203bba344e4704dd7daaeed4b893
Villanueva-Midterm-Exam <justinvillanueva984@gmail.com>
2024-04-22 16:22:14 +0800

    input image

--- app/src/main/java/com/example/villanuevaapp/MainActivity.kt
+++ app/src/main/java/com/example/villanuevaapp/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.villanuevaapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.villanuevaapp.ui.theme.VillanuevaAPPTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +30,40 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                   VillanuevaApp()
                 }
             }
         }
     }
 }
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun VillanuevaApp() {
+    MyApp(
+        title = stringResource(R.string.title_note),
+        imagePainter = painterResource(R.drawable.notes)
     )
 }
 
+@Composable
+private fun MyApp(
+    title: String,
+    imagePainter: Painter,
+    modifier: Modifier = Modifier,
+) {
+    Column(modifier = modifier) {
+        Image(painter = imagePainter, contentDescription = null)
+        Text(
+            text = title,
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Justify
+        )
+
+    }
+}
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun VillanuevaAppPreview() {
     VillanuevaAPPTheme {
-        Greeting("Android")
+        VillanuevaApp()
     }
 }
