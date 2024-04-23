9d08f04a1a390c217ade6bf7a0f9601f2bb3fffb
VIllanuevaAPP <justinvillanueva984@gmail.com>
2024-04-23 15:49:25 +0800

    Updated

--- app/src/main/java/com/example/villanuevaapp/MainActivity.kt
+++ app/src/main/java/com/example/villanuevaapp/MainActivity.kt
@@ -6,11 +6,18 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
@@ -30,7 +37,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                   VillanuevaApp()
+                    VillanuevaApp()
                 }
             }
         }
@@ -48,22 +55,49 @@ fun VillanuevaApp() {
 private fun MyApp(
     title: String,
     imagePainter: Painter,
-    modifier: Modifier = Modifier,
+    modifier: Modifier = Modifier
+
 ) {
     Column(modifier = modifier) {
-        Image(painter = imagePainter, contentDescription = null)
+        Image(painter = imagePainter, contentDescription = null, modifier= Modifier.fillMaxWidth())
         Text(
             text = title,
-            modifier = Modifier.padding(16.dp),
-            textAlign = TextAlign.Justify
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(16.dp)
+                .weight(.1f)
+                .fillMaxWidth()
+
+
+
         )
 
+        SimpleFilledTextFieldSample( Modifier.weight(.8f))
     }
 }
+
+@Composable
+fun SimpleFilledTextFieldSample(modifier: Modifier) {
+    var text by remember { mutableStateOf("") }
+
+    TextField(
+        value = text,
+        onValueChange = { text = it },
+        label = { Text("What's in your mind") },
+        modifier = modifier.fillMaxSize()
+
+    )
+}
+
+
 @Preview(showBackground = true)
 @Composable
 fun VillanuevaAppPreview() {
     VillanuevaAPPTheme {
-        VillanuevaApp()
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            VillanuevaApp()
+        }
     }
 }
